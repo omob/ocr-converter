6c85542c1923473ad3ddd46fb80ef93e6be41fc8
Ayodeji Abodunrin <aboayosam@gmail.com>
2020-04-22 00:43:24 +0100

    program can read text from scanned pdf and convert same to pdf

--- index.js
+++ index.js
@@ -1,27 +1,74 @@
 const fs = require("fs");
 const path = require("path");
 const tessaract = require("node-tesseract-ocr");
+const PDFDocument = require("pdfkit"); //create pdf document
+var pdftoimage = require("pdftoimage"); //converts pdf to image
 
-const PDFDocument = require("pdfkit");
+const filePath = "assets/test1.pdf";
+const tempDirName = path.basename(filePath, path.extname(filePath));
+const tempDirPath = path.join(__dirname, `assets/temp/${tempDirName}`);
+const outputPath = path.join(__dirname, `assets/outputs/${tempDirName}`);
 
-const filePath = "assets/MemartWapicLife.pdf";
-const outputPath = path.join(__dirname, "test.pdf");
+//convert to image
+const convertToImages = async (filePath, tempPath) => {
+  fs.mkdir(tempPath, () => {});
 
-const convertToPdf = (text, outputPath) => {
+  try {
+    await pdftoimage(filePath, {
+      format: "png", // png, jpeg, tiff or svg, defaults to png
+      prefix: "img", // prefix for each image except svg, defaults to input filename
+      outdir: tempPath, // path to output directory, defaults to current directory
+    });
+  } catch (e) {
+    console.log("Error converting pdf to image. \n", e);
+  }
+};
+
+// convertToImage(tempDirPath);
+
+const getTextFromImages = async (tempDirPath) => {
+  // read each image file and extract text
+  const filesName = fs.readdirSync(tempDirPath);
+  const compiledTexts = [];
+  for (const [index, file] of filesName.entries()) {
+    try {
+      const result = await tessaract.recognize(`${tempDirPath}\\${file}`);
+      compiledTexts.push({
+        text: result,
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+  return compiledTexts;
+};
+
+const convertTextsToPdf = (texts, outputPath) => {
   const pdfDoc = new PDFDocument();
+  pdfDoc.pipe(fs.createWriteStream(outputPath + ".pdf"));
 
-  pdfDoc.pipe(fs.createWriteStream(outputPath));
-  pdfDoc.text(text);
+  for (const text of texts) {
+    pdfDoc.text(text.text);
+  }
   pdfDoc.end();
+};
+
+const clearTempFolder = () => {
+  fs.rmdir(tempDirPath, { recursive: true }, () => {});
 };
 
 const run = async () => {
   try {
-    const result = await tessaract.recognize(filePath);
-    console.log(result);
-    // convertToPdf(result, outputPath);
+    // convert pdf to images
+    await convertToImages(filePath, tempDirPath);
+    const readTexts = await getTextFromImages(tempDirPath);
+    convertTextsToPdf(readTexts, outputPath);
+
+    clearTempFolder();
+
+    console.log("Done");
   } catch (e) {
-    console.log(e.message);
+    console.log("Something happened: ", e);
   }
 };
 
